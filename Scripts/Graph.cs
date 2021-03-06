ad0f445b7713331bae09306b22e589c62ee04027
FracturedDiamond <49692399+FracturedDiamond@users.noreply.github.com>
2021-03-05 23:32:49 -0700

    Scripts

---
+++ Scripts/Graph.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph
+{
+    List<Connection> mConnections;
+
+    // an array of connections outgoing from the given node
+    public List<Connection> getConnections(Node fromNode)
+    {
+        List<Connection> connections = new List<Connection>();
+        foreach (Connection c in mConnections)
+        {
+            if (c.getFromNode() == fromNode)
+            {
+                connections.Add(c);
+            }
+        }
+        return connections;
+    }
+
+    public void Build()
+    {
+        // find all nodes in scene
+        // iterate over the nodes
+        //   create connection objects,
+        //   stuff them in mConnections
+        mConnections = new List<Connection>();
+
+        Node[] nodes = GameObject.FindObjectsOfType<Node>();
+        foreach (Node fromNode in nodes)
+        {
+            foreach (Node toNode in fromNode.ConnectsTo)
+            {
+                if (toNode.tollRoad == true && toNode.teleporter == false)
+                {
+                    toNode.cost = 50;
+                    Debug.Log(toNode.cost);
+                }
+                else if (toNode.tollRoad == false && toNode.teleporter == true)
+                {
+                    toNode.cost = 0;
+                    Debug.Log(toNode.cost);
+                } else
+                {
+                    toNode.cost = (toNode.transform.position - fromNode.transform.position).magnitude;
+                    Debug.Log(toNode.cost);
+                }
+
+                Connection c = new Connection(toNode.cost, fromNode, toNode);
+                mConnections.Add(c);
+            }
+        }
+    }
+}
+
+public class Connection
+{
+    float cost;
+    Node fromNode;
+    Node toNode;
+
+    public Connection(float cost, Node fromNode, Node toNode)
+    {
+        this.cost = cost;
+        this.fromNode = fromNode;
+        this.toNode = toNode;
+    }
+    public float getCost()
+    {
+        return cost;
+    }
+
+    public Node getFromNode()
+    {
+        return fromNode;
+    }
+
+    public Node getToNode()
+    {
+        return toNode;
+    }
+}
---
+++ Scripts/Node.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Node : MonoBehaviour
+{
+    public Node[] ConnectsTo;
+    public float cost;
+    public bool tollRoad = false;
+    public bool teleporter = false;
+
+    public Text theText;
+
+    public GameObject sphere;
+
+    public void Teleport()
+    {
+        teleporter = !teleporter;
+
+        if (teleporter)
+        {
+            theText.text = "Teleport: 0";
+            theText.color = Color.magenta;
+        } else if (tollRoad)
+        {
+            theText.text = "Toll Road: 50";
+            theText.color = Color.red;
+        } else
+        {
+            theText.text = "Road: ~15";
+            theText.color = new Color32(32, 32, 32, 255);
+        }
+
+        sphere.GetComponent<Pathfinder>().Restart();
+    }
+
+    public void Toll()
+    {
+        tollRoad = !tollRoad;
+
+        if (teleporter)
+        {
+            theText.text = "Teleport: 0";
+            theText.color = Color.magenta;
+        }
+        else if (tollRoad)
+        {
+            theText.text = "Toll Road: 50";
+            theText.color = Color.red;
+        }
+        else
+        {
+            theText.text = "Road: ~15";
+            theText.color = new Color32(32, 32, 32, 255);
+        }
+
+        sphere.GetComponent<Pathfinder>().Restart();
+
+
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        foreach (Node n in ConnectsTo)
+        {
+            Gizmos.color = Color.gray;
+            Gizmos.DrawLine(transform.position, n.transform.position);
+            //Gizmos.DrawRay(transform.position, (n.transform.position - transform.position).normalized * 2);
+        }
+    }
+
+}
